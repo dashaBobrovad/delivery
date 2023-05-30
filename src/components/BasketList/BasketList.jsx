a115c34104da2269c205f0b7bf91bd67bd43ba25
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-05-30 22:34:46 +0300

    remove btns hover

--- src/components/BasketList/BasketList.jsx
+++ src/components/BasketList/BasketList.jsx
@@ -1,6 +1,7 @@
 import s from './BasketList.module.scss';
 import cx from 'classnames';
 import { Icon } from 'components';
+import { Link } from 'react-router-dom';
 
 // TODO: use Sprite
 // TODO: разнести на компоненты
@@ -36,10 +37,7 @@ function BasketList(params) {
           {basketList.map((pizza, index) => (
             <div className={s.item} key={pizza.id}>
               <div className={s.item__img}>
-                <img
-                  src={pizza.imageUrl}
-                  alt="Pizza"
-                />
+                <img src={pizza.imageUrl} alt="Pizza" />
               </div>
               <div className={s.item__info}>
                 {/* TODO: по id ? */}
@@ -53,7 +51,7 @@ function BasketList(params) {
                     s.item__count_minus
                   )}
                 >
-                 <Icon icon="minus" />
+                  <Icon icon="minus" color="primary" />
                 </div>
                 <b>2</b>
                 <div
@@ -62,7 +60,7 @@ function BasketList(params) {
                     s.item__count_plus
                   )}
                 >
-                  <Icon icon="plus" />
+                  <Icon icon="plus" color="primary" />
                 </div>
               </div>
               <div className={s.item__price}>
@@ -70,22 +68,7 @@ function BasketList(params) {
               </div>
               <div className={s.item__remove}>
                 <div className="button button--outline button--circle">
-                  <svg
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                      fill="#EB5A1E"
-                    />
-                    <path
-                      d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                      fill="#EB5A1E"
-                    />
-                  </svg>
+                  <Icon icon="cross" color="primary" />
                 </div>
               </div>
             </div>
@@ -103,28 +86,15 @@ function BasketList(params) {
             </span>
           </div>
           <div className={s.bottom__buttons}>
-            <a
-              href="/"
+            <Link
+              to="/"
               className={cx('button button--outline button--add', s.back)}
             >
-              <svg
-                width="8"
-                height="14"
-                viewBox="0 0 8 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 13L1 6.93015L6.86175 1"
-                  stroke="#D3D3D3"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
+              <Icon icon="grey-arrow-left" />
 
               <span>Вернуться назад</span>
-            </a>
+            </Link>
+
             <div className={cx('button', s.pay)}>
               <span>Оплатить сейчас</span>
             </div>
