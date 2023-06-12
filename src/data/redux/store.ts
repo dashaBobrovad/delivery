434d625ee98dee9416119afbe37b7c50714aea4f
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-06-12 17:06:04 +0300

    add local storage save to store

--- src/data/redux/store.ts
+++ src/data/redux/store.ts
@@ -1,11 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pizzasReducer from "./features/pizzas/pizzasSlice";
 
-export const store = configureStore({
+function saveToLocalStorage(state:RootState){
+
+  try{
+    const serialState = JSON.stringify(state)
+    localStorage.setItem("reduxStore",serialState)
+  }catch(e){
+    console.warn(e);
+  }
+}
+
+function loadFromLocalStorage(){
+
+  try{
+    const serialisedState = localStorage.getItem("reduxStore");
+    if(serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  }catch(e){
+    console.warn(e);
+    return undefined;
+  }
+}
+
+const store = configureStore({
   reducer: {
     pizzas: pizzasReducer,
   },
+  preloadedState:loadFromLocalStorage()
 });
+
+store.subscribe(()=>saveToLocalStorage(store.getState()));
+
+export {store};
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
