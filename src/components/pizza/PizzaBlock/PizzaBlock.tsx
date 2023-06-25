ca5ad4bb3ef6bf7348003e3924ef3d5ef720cf9f
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-06-25 12:20:05 +0300

    fx ids array

--- src/components/pizza/PizzaBlock/PizzaBlock.tsx
+++ src/components/pizza/PizzaBlock/PizzaBlock.tsx
@@ -18,6 +18,9 @@ function PizzaBlock({ pizza }: IPizzaBlockProps) {
 
   const dispatch = useTypedDispatch();
 
+  const basketPizza = useTypedSelector((state) => state.pizzas.basket.list);
+  const [pizzaCount, setPizzaCount] = useState<number>(0);
+
   const [activeType, setActiveType] = useState(0);
   const [activeSize, setActiveSize] = useState(0);
 
@@ -30,8 +33,7 @@ function PizzaBlock({ pizza }: IPizzaBlockProps) {
   };
 
   const onAddToBasket = () => {
-    //  dispatch(addToBasket({ id, type: activeType, size: activeSize }));
-    // TODO: не надо передавать все
+    // TODO: заменить на id (?)
     const item = {
       id,
       title,
@@ -43,62 +45,21 @@ function PizzaBlock({ pizza }: IPizzaBlockProps) {
     dispatch(addToBasket(item));
   };
 
-  // const basketPizza = useTypedSelector((state) =>
-  //   state.pizzas.basket.list.find((item) => item.id === id)
-  // );
-  //  const arr = [] as any[];
-  // const basketPizza = arr.push(
-  //   useTypedSelector((state) =>
-  //     state.pizzas.basket.list.find((item) => item.id === id)
-  //   )
-  // );
-
-  const basketPizza = useTypedSelector((state) => state.pizzas.basket.list);
-
-  const [res, setRes] = useState<any>(0);
-
   useEffect(() => {
-    // basketPizza.find((item) => item.)
-    //  const foundP = basketPizza.find((item) => item.id === id);
-    //  if(foundP){
-    //   console.log(`${foundP.title  }:${   foundP.count}`);
-    //  }
-
-    const newArr = [] as any[];
-    Object.keys(basketPizza).map((el) =>
-      newArr.indexOf(basketPizza[el as any].id) !== -1
-        ? null
-        : newArr.push({
-            id: basketPizza[el as any].id,
-            count: basketPizza[el as any].count,
-          })
+    const basketItemsArr = [] as { id: number }[];
+    Object.keys(basketPizza).forEach((el) =>
+      basketItemsArr.push({
+        id: basketPizza[Number(el)].id,
+      })
     );
 
-    // объединить все объекты с одинаковым id и в редьюсе посчитать их сумму
+    const filteredArr = [] as { id: number }[];
+    basketItemsArr.forEach((item) => item.id === id && filteredArr.push(item));
 
-    const resArr = [] as any[];
-    newArr.forEach((item) => item.id === id && resArr.push(item));
-   //  console.log(resArr);
-    // newArr.filter((item) => item.id === 1);
-   // console.log(newArr);
-   // const newArrObj = [] as any[];
-     // Object.keys(newArr).map((el) => newArrObj.push({ id: el }));
+    const pizzaCount = filteredArr.reduce((sum, obj: any) => obj.count + sum, 0);
 
-    // console.log(JSON.stringify(newArrObj));
-
-    const ttt = resArr.reduce((sum, obj: any) => obj.count + sum, 0);
-
-    console.log(resArr);
-
-  setRes(ttt);
+    setPizzaCount(pizzaCount);
   }, [basketPizza]);
-
-  useEffect(() => {
-   console.log(res);
-  }, [res]);
-  
-  // const pizzaCount = basketPizza;
-  // console.log(pizzaCount);
 
   return (
     <div className={s.pizza}>
@@ -147,7 +108,7 @@ function PizzaBlock({ pizza }: IPizzaBlockProps) {
         >
           <Icon icon="plus" color="primary" />
           <span>Добавить</span>
-          <i>{res}</i>
+          <i>{pizzaCount || 0}</i>
         </button>
       </div>
     </div>
